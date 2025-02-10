a4aebb56508c961ddc70718a65a280082c31bda9
Andrii-Detix <aivancisin6@gmail.com>
2025-02-10 21:59:49 +0200

    Add timeHandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,10 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 type Handler struct {
-	Methods []string
+	Methods    []string
 	Controller func(http.ResponseWriter, *http.Request)
+}
+
+func timeHandler(response http.ResponseWriter, request *http.Request) {
+	currentTime := time.Now().Format(time.RFC3339)
+	fmt.Println(currentTime)
 }
