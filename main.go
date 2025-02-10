597e6d8c57ceab0bc7ed47cf4dbf83e2cdb24142
general_maks <schrinan@gmail.com>
2025-02-10 22:55:43 +0200

    Update server setup

--- main.go
+++ main.go
@@ -13,7 +13,18 @@ const port = "8795"
 func main() {
 	address := net.JoinHostPort(host, port)
 	for path, handler := range handlers.Handlers {
-		http.HandleFunc(path, handler.Controller)
+		controller := func(response http.ResponseWriter, request *http.Request) {
+			for _, method := range handler.Methods {
+				if method == request.Method {
+					handler.Controller(response, request)
+					return
+				}
+				response.WriteHeader(http.StatusMethodNotAllowed)
+				message := "Method " + request.Method + " not allowed"
+				response.Write([]byte(message))
+			}
+		}
+		http.HandleFunc(path, controller)
 	}
 	fail := http.ListenAndServe(address, nil)
 	log.Fatal(fail)
